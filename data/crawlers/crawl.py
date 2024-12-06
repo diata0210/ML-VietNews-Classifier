99f8ab5a4944b0c0f237b57d4154dd86b55ede47
Đỗ Bảo <baba7cvk@gmail.com>
2024-12-06 18:24:45 +0700

    new crawl data

--- data/crawlers/crawl.py
+++ data/crawlers/crawl.py
@@ -122,4 +122,4 @@ def main(category, base_url, start_page, end_page):
 
 # Ví dụ gọi hàm main cho thể loại 'technology', từ trang 6 đến trang 36
 
-main("technology", "https://vnexpress.net/so-hoa/cong-nghe", 2, 20)
+main("entertaiment", "https://vnexpress.net/giai-tri", 2, 20)
